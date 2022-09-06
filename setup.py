40de406ab5a4a70bd92d8d25797131612f77a74f
brodokk <brodokk@brodokk.space>
2022-09-06 22:40:48 +0200

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+    name='neos.py',
+    version='0.1',
+    description='NeosVR API module',
+    author='Alexander Terry, brodokk',
+    author_email='alexterry43@gmail.com, brodokk@brodokk.space',
+    url='https://github.com/brodokk/neos.py',
+    packages=['neos'],
+)
