8a2922f0fbe63250c357752612d33eda749ca4cb
brodokk <brodokk@brodokk.space>
2023-09-16 01:59:24 +0200

    Also update token lastupdate

--- neosvrpy/client.py
+++ neosvrpy/client.py
@@ -103,6 +103,7 @@ class Client:
             # From PolyLogix/CloudX.js, the token seems to expire after 3600000 seconds
             if (datetime.now() - lastUpdate).total_seconds() <= 3600000:
                 self._request('patch', '/userSessions', ignoreUpdate=True)
+                self.lastUpdate = datetime.now()
             # While the API dont seems to implement more security, official client behavior must be respected.
             # Only disconnect after 1 day of inactivity for now.
             # TODO: Implement disconnection after 1 week of inactivity when implementing the rememberMe feature.
